b9da5bcd65f7926c07d723bceccb0836c08529fa
FelipeAndresLopez <felipe.lopez@uptc.edu.co>
2024-03-07 15:06:39 -0500

    primer commit, creación de entity, conexión y creación de tablas de la base de datos, se agregan dependencias jjwt (pom).

---
+++ src/main/java/com/srt/servicioreistrotecnologico/security/springsecurityjwt/entity/usuarios.java
@@ -0,0 +1,30 @@
+package com.srt.servicioreistrotecnologico.security.springsecurityjwt.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class usuarios {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+    private String username;
+    private String password;
+    //*  (fetch.eager) cada que consulta a la base de datos trae todos los roles
+    //* (cascade.all) cuando elimina un usuario elimina toda la data
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "usuarios_roles", joinColumns = @JoinColumn(
+            name = "usuario_id", referencedColumnName = "id_usuario")
+            ,inverseJoinColumns = @JoinColumn (name = "role_id",referencedColumnName = "id_role"))
+    private List<roles> roles = new ArrayList<>();
+}
